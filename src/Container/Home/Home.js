998270c26bb7680898664454e5aaf8a1c3986a7f
TechExplified <vaibhavirohilla741@gmail.com>
2022-09-09 11:06:56 +0530

    reach page

--- src/Container/Home/Home.js
+++ src/Container/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import CarouselContainer from "../../components/Carousel/CarouselContainer";
 import OurServices from "../../components/OurServices/OurServices";
+import Reach from "../../components/Reach/Reach";
 
 const Home = () => {
   return (
     <>
       <CarouselContainer />
+      <Reach/>
       <OurServices />
+     
     </>
   );
 };
---
+++ src/components/Reach/Reach.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Reach.css";
+
+function Reach() {
+  return (
+    <div className="reachContainer">
+      <div className="reachContent">
+        <div className="contentheading">
+          <div>
+            <h2> Skills and experience that shape eCommerce</h2>
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum. ​Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat.
+            </p>
+          </div>
+
+          <div className="flexbox">hello</div>
+        </div>
+        <div>
+          <h3>
+            We've helped businesses increase their revenue on average by 90% in
+            their first year with us!
+          </h3>
+          <p>Sample text. Click to select the Text Element.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Reach;
