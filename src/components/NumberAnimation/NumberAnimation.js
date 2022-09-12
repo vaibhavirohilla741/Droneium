151dec8763c8cf062e9395331845be1055382291
TechExplified <vaibhavirohilla741@gmail.com>
2022-09-13 00:41:10 +0530

    number ui

---
+++ src/components/NumberAnimation/NumberAnimation.js
@@ -0,0 +1,32 @@
+import AnimatedNumber from "animated-number-react";
+import "./NumberAnimation.css";
+import styled from "styled-components";
+
+import React, { useState } from "react";
+
+function NumberAnimation(props) {
+  const NumberContainer = styled.div`
+    height: 25vh;
+    background-color: ${props.color};
+    color: ${props.textColor};
+    margin: 3px;
+  `;
+
+  const [value, setValue] = useState(props.value);
+  let formatValue = (value) => value.toFixed(0);
+  return (
+    <NumberContainer>
+      <center>
+        <AnimatedNumber
+          value={value}
+          formatValue={formatValue}
+          duration={3000}
+          easing={"linear"}
+          className={"numberAnimation"}
+        ></AnimatedNumber><p style={{margin:"none"}}>Iam Genius!</p>
+      </center>
+    </NumberContainer>
+  );
+}
+
+export default NumberAnimation;
--- src/components/Reach/Reach.js
+++ src/components/Reach/Reach.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NumberAnimation from "../NumberAnimation/NumberAnimation";
 import "./Reach.css";
 
 function Reach() {
@@ -7,23 +8,34 @@ function Reach() {
       <div className="reachContent">
         <div className="contentheading">
           <div>
-            <h2> Skills and experience that shape eCommerce</h2>
+            <h2> Fly safely and responsibly, folks. </h2>
             <p>
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit anim id est laborum. ​Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat.
+              Whether you’re an aspiring aerial videographer, a land surveyor,
+              construction site mapper, wildlife tracker, or simply an Unmanned
+              Aerial Vehicle (UAV) / drone hobbyist, let’s be honest there are
+              few greater things in life than owning your own drone. Right?
             </p>
           </div>
-
-          <div className="flexbox">hello</div>
         </div>
         <div>
           <h3>
-            We've helped businesses increase their revenue on average by 90% in
-            their first year with us!
+            The first thing you should know about buying a drone is that you
+            don’t have to buy one.
           </h3>
-          <p>Sample text. Click to select the Text Element.</p>
+          <p>
+            The best way to learn more about the UAV industry is to pilot your
+            own drone.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="flexbox">
+        <div className="flexbox1">
+        <NumberAnimation value={100} color='#f1c50e' textColor='#111111'/>
+        <NumberAnimation value={200} color='#333333' textColor="#ffffff" />
+        </div>
+        <div className="flexbox1">
+          <NumberAnimation value={300} color='#333333' textColor="#ffffff"/>
+          <NumberAnimation value={400} color='#f1c50e' textColor='#111111'/>
         </div>
       </div>
     </div>
