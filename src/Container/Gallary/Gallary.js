bceb53cc4bb51d3de292cca2c82c2914088156b2
TechExplified <vaibhavirohilla741@gmail.com>
2022-09-30 12:11:47 +0530

    Gallary working

--- src/Container/Gallary/Gallary.js
+++ src/Container/Gallary/Gallary.js
@@ -1,21 +1,52 @@
+import React, { useState } from "react";
+
 import PhotoAlbum from "react-photo-album";
 
-import React from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "./Gallary.css"
 
-function Gallary() {
-  const photos = [
-    {
-      src: "https://th.bing.com/th/id/R.ca6c02159e31328594f13f3debdbe41c?rik=KqIcu4myXOsBQQ&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fwp1816232.jpg&ehk=HVL9PYRl5Xgi%2bRdPIRDK3ojbijbpQppTek6AmGC%2bE9Y%3d&risl=&pid=ImgRaw&r=0",
-      width: 800,
-      height: 600,
-    },
-    {
-      src: "https://th.bing.com/th/id/R.18b66c3b36872b25cfebf84d9f9dc401?rik=c%2fYa6w6%2fBtJS3w&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fwp1816233.jpg&ehk=CDaqV8cnYM0FZMICxz%2fZdSnevAKZd0AO000p%2bBEvkgs%3d&risl=&pid=ImgRaw&r=0",
-      width: 1600,
-      height: 900,
-    },
-  ];
-  return <PhotoAlbum layout="rows" photos={photos} />;
-}
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/dist/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/dist/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/dist/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/dist/plugins/zoom";
+import "./Thumbnails.css";
+
+import photos from "./photos";
+
+const slides = photos.map(({ src, width, height, images }) => ({
+    src,
+    width,
+    height,
+    srcSet: images.map((image) => ({
+        src: image.src,
+        width: image.width,
+        height: image.height,
+    })),
+}));
+
+const Gallary = () => {
+    const [index, setIndex] = useState(-1);
+
+    return (
+        <>
+            <PhotoAlbum
+                photos={photos}
+                layout="rows"
+                targetRowHeight={150}
+                onClick={(event, photo, index) => setIndex(index)}
+            />
+
+            <Lightbox
+                slides={slides}
+                open={index >= 0}
+                index={index}
+                close={() => setIndex(-1)}
+                // enable optional lightbox plugins
+                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+            />
+        </>
+    );
+};
 
 export default Gallary;
